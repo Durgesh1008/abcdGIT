41a09183a73379241a3de1e97fd730dca499afbc
Durgesh1008 <dk15708012@gmail.com>
2022-02-08 23:27:21 +0530

    git push

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -20,5 +20,36 @@ a = null;
 console.log(a);
 console.group(typeof a)
 
+//Number
+
+var num = 10;
+console.log(num);
+var num = 2.4;
+console.log(typeof num);
+
+//bullioon
+
+var t = true;
+console.log(t);
+var f = false;
+console.log(f);
+
+//string
+console.log(typeof "Jay Shree Ram")
+
+//use of backslesh for break the line use(\n)
+str = "How you doin ?\n i am fine and you? "
+console.log(str);
+
+//power of back tick break line without ust \ 
+var cst = `hii IASHBH AHUASDC
+IIUCSGVCS IASDDHVCSAAJ
+CIUC  `
+console.log(cst);
+
+//dynmic input 
+var num = 1441;
+console.log(`Half of ${num} is ${num/2}`);
+
 
 console.log("Ram JI KI Sarkar")
