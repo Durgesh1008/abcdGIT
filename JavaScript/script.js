19cfb99938441ca479b62761f9602e6b49496a55
Durgesh1008 <dk15708012@gmail.com>
2022-02-08 22:55:58 +0530

    javaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,24 @@
+
+
+
+var  a;
+
+a = 10;
+console.log(a);
+
+a = "Hello Durgesh"
+console.log(a)
+console.log(typeof a)
+
+
+a = true;
+console.log(a)
+console.log(typeof a)
+
+
+a = null;
+console.log(a);
+console.group(typeof a)
+
+
+console.log("Ram JI KI Sarkar")
