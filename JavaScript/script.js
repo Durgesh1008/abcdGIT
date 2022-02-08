982117ae9b6b0203dffcb4ac5b59459e58088347
Durgesh1008 <dk15708012@gmail.com>
2022-02-09 00:01:50 +0530

    basicJSdone

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -53,3 +53,43 @@ console.log(`Half of ${num} is ${num/2}`);
 
 
 console.log("Ram JI KI Sarkar")
+
+
+//redeclaration is allowe  but this is Wrong
+var l = hello;
+console.log(l);
+var l = 200;
+console.log(l);
+
+//over come krne kai liyai
+//comes let veriabels
+let l = 100;
+console.log(l);
+ 
+//we can reintialization a veraiable but we cannot redeclara it
+//  l = 300;
+// console.log(l);  //SyntaxError: Identifier 'l' has already been declared
+
+//loops in Js
+
+let numb = 10;
+for(var i = 0; i < num; i++){
+    if(i % 2 == 0){
+        console.log("input is even");
+    }
+}
+
+
+//const
+//no reintialization and no redeclared
+
+const a = 100;
+console.log(a);
+
+const a = "100"
+console.log(a) // note working
+
+a = 100;
+console.log(a) // not working
+
+
